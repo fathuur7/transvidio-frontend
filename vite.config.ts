7d9c894c19a2f6b9533def529687715baf0412cb
Nur fathur rachman <230411100035@student.trunojoyo.ac.id>
2025-10-20 23:12:08 +0700

    Update Vite server host configuration
    Change server host configuration to allow external access.

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
+   server: {
+    host: true, // biar bisa diakses dari luar
     port: 8080,
-  },
+    allowedHosts: ['srv1068768.hstgr.cloud'] 
+  }
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
